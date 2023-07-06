3d53bf388555f722a3fd48f6d19a67d79d1ef00a
DarwinDontCare <117847214+DarwinDontCare@users.noreply.github.com>
2023-07-06 12:10:10 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
 function App() {
+
+  var [date, setDate] = React.useState(new Date());
+  const [count, setCount] = React.useState(0);
+  var [apocalipseDate, setApocalipseDate] = React.useState(new Date(0, 0, 1, 0, 0, 0));
+
+  React.useEffect(() => {
+    var timer = setInterval(() => setDate(new Date()), 1000);
+
+    return function cleanup() {
+      clearInterval(timer);
+    }
+  });
+
+  function ClockComponent() {
+    return (
+        <div className='clock-div'>
+          <p>Current Time: {date.getHours()}: {date.getMinutes()}: {date.getSeconds()}</p>
+        </div>
+      );
+  }
+
+  function Counter() {
+    return (
+      <div className='counter-div'>
+        <span>Time to the APOCALYPSE: {apocalipseDate.getDate()} days, {apocalipseDate.getHours()} hours, {apocalipseDate.getMinutes()} minutes and {apocalipseDate.getSeconds()} seconds</span>
+        <br></br>
+        <button onClick={() => {
+          setCount(count + 1);
+          setApocalipseDate(new Date(0, 0, 1, 0, 0, count));
+        }}>Clique aqui</button>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='vignette'></div>
+      <ClockComponent/>
+      <Counter/>
     </div>
   );
 }
